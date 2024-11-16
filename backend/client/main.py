ca027927ffd13aabe2dee26f3910fc5792a039e1
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-11-17 00:54:59 +0100

    Added client args to separate module

--- backend/client/main.py
+++ backend/client/main.py
@@ -3,17 +3,7 @@ import uvicorn
 import argparse
 from routers import transactions
 from routers import network
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Run a FastAPI application with argparse")
-    parser.add_argument("--host", type=str, default="localhost", help="Host to run the FastAPI app on")
-    parser.add_argument("--port", type=int, default=8000, help="Port to run the FastAPI app on")
-    parser.add_argument("--reload", action="store_true", help="Enable auto-reload during development")
-    parser.add_argument("--id", type=str, default="client0", help="ID of the peer")
-    
-    return parser.parse_args()
-
-args = parse_args() 
+from args import args
 
 app = FastAPI()
 app.include_router(network.router, prefix="/api/v1")
