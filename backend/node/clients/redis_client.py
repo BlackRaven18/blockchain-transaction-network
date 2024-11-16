10de27b3a1024846f97533d87d42d5b9f5f792db
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-11-16 23:21:13 +0100

    Updated redis client

--- backend/node/clients/redis_client.py
+++ backend/node/clients/redis_client.py
@@ -1,11 +1,23 @@
 import redis
+from schemas.blockchain import Blockchain
+from config import args
 
 class RedisClient:
-    def __init__(self, host, port):
-        self.client = redis.Redis(host=host, port=port)
+    _client = None
 
-    def set(self, key, value):
-        self.client.set(key, value)
+    @classmethod
+    def __init_db(cls):
+        if cls._client is not None:
+            cls._client.set('blockchan', Blockchain().model_dump_json())
 
-    def get(self, key):
-        return self.client.get(key)
+    @classmethod
+    def get_client(cls):
+        if cls._client is None:
+            host = args.db_host
+            port = args.db_port
+            try:
+                cls._client = redis.Redis(host=host, port=port, decode_responses=True)
+                cls.__init_db()
+            except ConnectionError as e:
+                print(f"Error connecting to Redis server: {e}")
+        return cls._client
