3f65cffe75b9c888456ba2d6783e9efe0c9e4061
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-11-20 16:26:00 +0100

    Added method to mine block

--- backend/node/schemas/block.py
+++ backend/node/schemas/block.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
 import datetime
 import hashlib
+import time
 from schemas.transaction import Transaction
-
 
 class Block(BaseModel):
     index: int
     timestamp: datetime.datetime = datetime.datetime.now()
     transactions: list[Transaction]
+    nonce: int = 0
     hash: str
     previous_block_hash: str
 
@@ -18,3 +19,15 @@ class Block(BaseModel):
     def calculate_hash(self):
         block_string = self.model_dump_json().encode()
         return hashlib.sha256(block_string).hexdigest()
+    
+    def mine_block(self, difficulty):
+        target = '0' * difficulty
+        start_time = time.time()
+
+        while self.hash[:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
+        end_time = time.time()
+        print(f"Block mined: {self.hash} (Nonce: {self.nonce})")
+        print(f"Mining took {end_time - start_time:.4f} seconds\n")
