faf24aeed6619bc295f560b8b06e1fc8887f9f62
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-11-27 12:45:32 +0100

    Updated creating genesis block

--- backend/node/schemas/block.py
+++ backend/node/schemas/block.py
@@ -12,18 +12,14 @@ class Block(BaseModel):
     timestamp: datetime.datetime = datetime.datetime.now()
     transactions: list[Transaction]
     nonce: int = 0
-    hash: str
+    hash: str = ""
     previous_block_hash: str
-
-    def model_post_init(self, __context):
-        if self.index != 1:
-            self.hash = self.calculate_hash()
 
     def calculate_hash(self):
         block_string = self.model_dump_json().encode()
         return hashlib.sha256(block_string).hexdigest()
     
-    async def mine_block(self, difficulty):
+    async def mine_block(self, difficulty) -> "Block":
         target = '0' * difficulty
         start_time = time.time()
 
@@ -36,4 +32,4 @@ class Block(BaseModel):
         print(f"Block mined: {self.hash} (Nonce: {self.nonce})")
         print(f"Mining took {end_time - start_time:.4f} seconds\n")
 
-        return self.model_dump_json()
+        return self
--- backend/node/schemas/blockchain.py
+++ backend/node/schemas/blockchain.py
@@ -8,26 +8,29 @@ class Blockchain(BaseModel):
     chain: list[Block] = []
     current_transactions: list[Transaction] = []
 
-    def create_genesis_block(self) -> None:
-        self.create_block(previous_hash='1')
-
-    # Create genesis block if blockchain is empty
     def model_post_init(self, __context: Any) -> None:
         if len(self.chain) == 0: 
-            genesis_block = self.create_block(previous_hash='1')
-            self.add_block(genesis_block)
+            self.__create_genesis_block()
 
-    def create_block(self, previous_hash=None) -> Block:
-        print("Creating a new block...")
-        
-        if previous_hash is None:
-            previous_hash = self.last_block.hash
+    def __create_genesis_block(self):
+        print("Creating genesis block...")
+
+        genesis_block = Block(
+            index=0,
+            hash="1",
+            transactions=[],
+            previous_block_hash="1"
+        )
+
+        self.add_block(genesis_block)
+
+    def create_block(self) -> Block:
 
         block = Block(
             index=len(self.chain) + 1,
-            hash="", # will be calculated later
+            hash="", # will be calculated during the mining process
             transactions=self.current_transactions,
-            previous_block_hash=previous_hash
+            previous_block_hash=self.last_block.hash
         )
 
         self.current_transactions = []
@@ -45,10 +48,6 @@ class Blockchain(BaseModel):
         )
 
         self.current_transactions.append(transaction)
-
-        #TODO: delete this later
-        # if len(self.current_transactions) >= get_config()["max_block_size"]:
-        #     self.create_block(self.last_block.hash)
 
     def should_mine_block(self) -> bool:
         return len(self.current_transactions) >= get_config()["max_block_size"]
