a8532657ba517a2d4512001b1f15191e3f1424b4
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-12-02 21:08:42 +0100

    Added network logger

---
+++ backend/logger/routers/websocket.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+from typing import List
+
+router = APIRouter()
+
+active_connections = []
+consumers: List[WebSocket] = []
+
+
+@router.websocket("/consume-logs")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await notify_consumers(data)
+
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
+
+
+@router.websocket("/register-consumer")
+async def forward(websocket: WebSocket):
+    await websocket.accept()
+    
+    consumers.append(websocket)
+    print("---Consumer registered---")
+
+    try:
+        while True:
+            await websocket.receive_text()
+
+    except WebSocketDisconnect:
+        consumers.remove(websocket)
+        print("---Consumer disconnected---")
+
+
+async def notify_consumers(message: str):
+    for consumer in consumers:
+        await send_message(consumer, message)
+
+
+async def send_message(websocket: WebSocket, message: str):
+    try:
+        await websocket.send_text(message)
+    except Exception as e:
+        print(f"Error sending message: {e}")
