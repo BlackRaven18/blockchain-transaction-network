e0b47f0e436e913550f162b0d68cc2313cf90732
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-12-04 12:29:46 +0100

    Updated node icons

--- frontend/src/flow/nodes/FloatingNode.tsx
+++ frontend/src/flow/nodes/FloatingNode.tsx
@@ -1,17 +1,27 @@
-import { Icon, Stack, styled, Typography } from '@mui/material';
+import { Divider, Icon, Stack, styled, Typography } from '@mui/material';
 import { Handle, Position, useConnection } from '@xyflow/react';
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import PowerOffIcon from '@mui/icons-material/PowerOff';
 
 interface NodeProps {
     id: string;
     data: any;
 }
 
-const AnimatedHourglass = styled(HourglassBottomIcon)({
+const BusyHourglass = styled(HourglassBottomIcon)({
     animation: "spin 2s linear infinite", // Define the animation
     "@keyframes spin": {
-      "0%": { transform: "rotate(0deg)" },
-      "100%": { transform: "rotate(360deg)" },
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(360deg)" },
+    },
+});
+
+const IdleHourglass = styled(HourglassBottomIcon)({
+    animation: "bounce 2s linear infinite", // Define the animation
+    "@keyframes bounce": {
+      "0%": { transform: "rotate(45deg)" },
+      "50%": { transform: "rotate(-45deg)" },
+      "100%": { transform: "rotate(45deg)" },
     },
   });
 
@@ -36,10 +46,13 @@ export default function FloatingNode({ id, data }: NodeProps) {
                 {(!connection.inProgress || isTarget) && (
                     <Handle className="customHandle" position={Position.Left} type="target" isConnectableStart={false} />
                 )}
-                <Stack direction={"column"}>
+                <Stack direction={"column"} style={{ alignItems: "center" }}>
                     {id}
+                    <Divider sx={{ width: "100%", bgcolor: "black" }} />
                     <Typography style={{ fontSize: "10px" }}>{"state:" + data?.state || "no info"}</Typography>
-                    <HourglassBottomIcon />
+                    {data?.state === "down" && <PowerOffIcon />}
+                    {data?.state === "idle" && <IdleHourglass />}
+                    {data?.state !== "idle" && data?.state !== "down" && <BusyHourglass />}
                 </Stack>
             </div>
         </div>
