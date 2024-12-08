28e8dccbd1c02e09fded99614ca14fb8b920dc91
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-12-08 19:04:41 +0100

    Added transaction recipient verification

--- backend/node/services/cryptography.py
+++ backend/node/services/cryptography.py
@@ -3,24 +3,29 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from schemas.transaction import Transaction
 from repositories.public_key import get_public_key
 
-
-def verify_transaction(transaction: Transaction) -> bool:
+def verify_transaction(transaction: Transaction) -> str:
 
     if not transaction.signature:
         print("No signature to verify")
-        return False  # No signature to verify
+        return "invalid"  # No signature to verify
     
-    public_key_pem = get_public_key(transaction.sender)
+    sender_public_key_pem = get_public_key(transaction.sender)
 
-    if public_key_pem is None:
+    if sender_public_key_pem is None:
         print("Public key not found")
-        return False
+        return "invalid"
     
-    public_key = serialization.load_pem_public_key(public_key_pem.encode('utf-8'))
+    receiver = get_public_key(transaction.recipient)
+
+    if receiver is None:
+        print("Receiver do not exist")
+        return "invalid"
+    
+    sender_public_key = serialization.load_pem_public_key(sender_public_key_pem.encode('utf-8'))
 
     serialized_data = transaction.serialize()
     try:
-        public_key.verify(
+        sender_public_key.verify(
             bytes.fromhex(transaction.signature),
             serialized_data.encode('utf-8'),
             padding.PSS(
